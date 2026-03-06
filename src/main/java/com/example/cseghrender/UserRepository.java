875af837625d22de3a8c33128f457b68d02ae22f
ritiksha-sharma <ritiksha380@gmail.com>
2026-03-06 10:02:57 +0530

    backend initial commit

---
+++ src/main/java/com/example/cseghrender/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.cseghrender;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<Users, Long> {
+    Users findByEmail(String email);
+
+}
